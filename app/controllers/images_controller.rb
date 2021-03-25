48941666a7bfbbe248da3dcafe820ad5af25aaec
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-03-25 16:21:37 +0900

    resthulな設計

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,35 @@
 class ImagesController < ApplicationController
+    before_action :set_image, only: %i[destroy]
+
     def index
+        images = Image.all.order(:id)
+        render json: images
+    end
+
+    def create
+        image = Image.new(post_params)
+        if image.save
+            render json: image
+        else
+            render json: image.errors
+        end
+    end
+
+    def destroy
+        if @image.destroy
+            render json: @image
+        else
+            render json: @image.errors
+        end
+    end
+
+    private
+
+    def set_image
+        @image = Image.find(params[:id])
+    end
+
+    def post_params
+        params.require(:params).permit(:label, :url)
     end
 end
